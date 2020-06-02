298cad44a96300ac316b1aaa67bee4d47d0c2b33
Torsten Motzkus <tmotzkus@web.de>
2020-06-02 18:38:45 +0200

    Fixed Seed, fixed teacher controller

--- db/migrate/20200602110443_create_timeslots.rb
+++ db/migrate/20200602110443_create_timeslots.rb
@@ -2,7 +2,7 @@ class CreateTimeslots < ActiveRecord::Migration[6.0]
   def change
     create_table :timeslots do |t|
       t.datetime :start_time
-      t.datetime :end_date
+      t.datetime :end_time
       t.boolean :booked, default: false
       t.references :availability, null: false, foreign_key: true
       t.references :student, index: true, foreign_key: { to_table: :users}
