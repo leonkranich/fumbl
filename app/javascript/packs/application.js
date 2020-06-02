28544eea95993edb8b13282c0ff378a40e1e5b98
Clement Bore <bore.clement@gmail.com>
2020-06-02 18:35:38 +0200

    components-2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,19 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initUpdateNavbarOnScroll();
 });
+
+// import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+// document.addEventListener('turbolinks:load', () => {
+//   // Call your JS functions here
+//   initUpdateNavbarOnScroll();
+// });
