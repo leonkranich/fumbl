1e0c81ab1c15bfc9daac5a611fe0b9c2aa6cde12
Markus Wahrheit <m.wahrheit@outlook.com>
2020-06-03 18:26:52 +0200

    dashboard backend

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,11 @@
 class DashboardsController < ApplicationController
-
   def index
-
+    @user = current_user
+    @timeslots = @user.timeslots_as_student
+    if params[:selected_id]
+    @selected_timeslot = @timeslots.find { |timeslot| timeslot.id == params[:selected_id].to_i }
+    else
+      @selected_timeslot = @timeslots.first
+    end
   end
 end
