1b656eef6759be1ae8ab8983d2243011a9d898b2
Clement Bore <bore.clement@gmail.com>
2020-06-09 18:00:46 +0200

    still working on the search but pushing that one

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,4 +1,15 @@
 class Instrument < ApplicationRecord
+  include PgSearch::Model
+  multisearchable against: [:name]
+  # include PgSearch::Model
+  # pg_search_scope :search, against: [ :name ],
+  # associated_against: {
+  #     user: [ :address ]
+  #   },
+  #   using: {
+  #     tsearch: { prefix: true }
+  # }
+
   belongs_to :user
 
   validates :user_id, presence: true
