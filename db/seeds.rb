2c6aeeefcb057883114e553175b05f63a78b7e2a
leon kranich <leon_kranich@yahoo.de>
2020-06-11 15:51:40 +0200

    prybybug

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require "date"
 require "time"
 require "nokogiri"
 require 'faker'
-require "pry-byebug"
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
